b39f4d14aa7600e41dda43bf8edf4602c957c962
SnowPhoenix <1595763343@qq.com>
2021-08-28 22:52:29 +0800

    update Script/python/utils/log to make it stronger

--- Scripts/python/utils/log.py
+++ Scripts/python/utils/log.py
@@ -3,40 +3,40 @@ import logging
 import time
 import sys
 
-class ShortLevelNameFormatter(logging.Formatter):
-    def __init__(self, *args, **kargs):
-        super().__init__(*args, **kargs)
-        tmp = {
+def config_logging(file, console_level: int=logging.INFO, file_level: int=logging.DEBUG):
+    _logger_trans = {
             "DEBUG": "DBG",
             "INFO": "INF",
             "WARNING": "WAR",
             "CRITICAL": "ERR"
         }
-        self.levelname_trans = dict({v:v for v in tmp.values()}, **tmp)
+    _old_factory = logging.getLogRecordFactory()
+    def factory(name, level, fn, lno, msg, args, exc_info, func=None, sinfo=None, **kwargs)->logging.LogRecord:
+        record = _old_factory(name, level, fn, lno, msg, args, exc_info, func, sinfo, **kwargs)
+        record.shortlevelname = _logger_trans[record.levelname]
+        return record
+    logging.setLogRecordFactory(factory)
 
-    def format(self, record: logging.LogRecord) -> str:
-        record.shortlevelname = self.levelname_trans[record.levelname]
-        return super().format(record)
-
-def config_logging(file, console_level: int=logging.INFO, file_level: int=logging.DEBUG):
     t = time.strftime("%Y_%m_%d", time.localtime(time.time()))
-
     file_handler = logging.FileHandler(pcat(Path.SCRIPTS_LOGS, f"{t}.log"), mode='a', encoding="utf8")
-    file_handler.setFormatter(ShortLevelNameFormatter('%(asctime)s [%(shortlevelname)s] %(module)s.%(lineno)d %(name)s:\t%(message)s'))
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s [%(shortlevelname)s] %(module)s.%(lineno)d %(name)s:\t%(message)s'
+        ))
     file_handler.setLevel(file_level)
 
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(ShortLevelNameFormatter(
+    console_handler.setFormatter(logging.Formatter(
         '[%(asctime)s %(shortlevelname)s] %(message)s',
-        datefmt="%Y/%m/%d %H:%M:%S"))
+        datefmt="%Y/%m/%d %H:%M:%S"
+        ))
     console_handler.setLevel(console_level)
 
     logging.basicConfig(
-        level=0,
+        level=min(console_level, file_level),
         handlers=[file_handler, console_handler],
         )
     
-    logging.info(f"running {file}")
+    logging.info(f"starting with file: {file}")
 
 if __name__ == '__main__':
     config_logging(__file__, logging.WARNING, logging.DEBUG)
@@ -44,3 +44,9 @@ if __name__ == '__main__':
     logging.info("info")
     logging.warning("warning")
     logging.critical("critical")
+
+    logger = logging.getLogger(__name__)
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.critical("critical")
